86f8d5adc1106bba7a75d94c05bc41a203e74a3c
FabiVelosa <a00267345@student.ait.ie>
2020-08-30 22:34:47 +0100

    junit tests

---
+++ GymAPP/src/com/ait/gym/test/GymClassTest.java
@@ -0,0 +1,16 @@
+package com.ait.gym.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GymClassTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+
+
+}
