f753447441f89022c354ce292ec96a48d380ebdf
FabiVelosa <a00267345@student.ait.ie>
2020-08-30 22:30:50 +0100

    trainer timetable page

---
+++ GymAPP/src/com/ait/gym/ClassesTypes.java
@@ -0,0 +1,24 @@
+package com.ait.gym;
+
+public enum ClassesTypes {
+	
+	PILATES(1,"Pilates"),YOGA(2,"Yoga"),TAI_CHI(3,"Tai-Chi"),ONE_ONE_SESSION(4,"1-1 Session"),MINDFULLNESS(5,"Mindfull"),CIRCUIT(6,"Circuits");
+	private final int classCode;
+	private final String value;
+	
+
+	private ClassesTypes(int classCode,String value) {
+	        this.classCode = classCode; 
+	        this.value = value;
+	        
+	  }
+	
+	 public int getClassCode() {
+	        return this.classCode;
+	 }
+
+	 public String getValue() {
+	        return this.value;
+	 }
+	 
+}
